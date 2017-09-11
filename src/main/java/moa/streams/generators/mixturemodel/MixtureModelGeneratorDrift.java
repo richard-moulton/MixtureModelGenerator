1530ace2bcd4613ef5c070e21c8be8f5f32f252f
richard-moulton <richard.hugh.m@gmail.com>
2017-09-11 14:07:17 -0400

    Initial commit for MixtureModelGeneratorDrift.
    Organized code in MixtureModel to group similiar methods together.
    Wrote MixtureModelGeneratorDrift, which will generate different mixture models to represent the stable concepts pre- and post-concept drift. Monte Carlo integration will be used to calculate the Hellinger distance between the mixture models to ensure they are the desired distance apart, within a desired threshold.

--- src/main/java/moa/streams/generators/mixturemodel/MixtureModelGeneratorDrift.java
+++ src/main/java/moa/streams/generators/mixturemodel/MixtureModelGeneratorDrift.java
@@ -22,6 +22,9 @@
 package moa.streams.generators.mixturemodel;
 
 import com.github.javacliparser.IntOption;
+import com.github.javacliparser.FlagOption;
+import com.github.javacliparser.FloatOption;
+import com.github.javacliparser.MultiChoiceOption;
 import com.yahoo.labs.samoa.instances.Attribute;
 import com.yahoo.labs.samoa.instances.Instance;
 import com.yahoo.labs.samoa.instances.Instances;
@@ -39,29 +42,62 @@ import moa.tasks.TaskMonitor;
  * 
  * @author Richard Hugh Moulton
  */
-public class MixtureModelGenerator extends AbstractOptionHandler implements InstanceStream
+public class MixtureModelGeneratorDrift extends AbstractOptionHandler implements InstanceStream
 {
+
 	private static final long serialVersionUID = 1L;
 
 	public IntOption numAttsOption = new IntOption("numAtts", 'a',
             "The number of attributes to generate.", 10, 0, Integer.MAX_VALUE);
 
-    public IntOption distributionOption = new IntOption("distribution", 'd',
+    public IntOption distributionOption = new IntOption("distribution", 'z',
             "Distribution used for models. Uniform 0, Gaussian 1, etc.",
             1, 0, 4);
     
-    public IntOption numClassesOption = new IntOption("numClasses", 's',
-            "The number of classes in the data stream and the number of models to include in the mixture model.",
+    public IntOption numClassesPreOption = new IntOption("numClassesPre", 'p',
+            "The number of classes in the data stream and the number of models to include in the mixture model pre-concept drift.",
+            2, 2, Integer.MAX_VALUE);
+    
+    public IntOption burnInInstances = new IntOption("burnInInstances", 'b',
+    		"The number of instances to draw from the pre-concept drift mixture model.", 10000, 1, Integer.MAX_VALUE);
+    
+    public IntOption driftDuration = new IntOption("driftDuration", 'd',
+    		"The number of instances between the stable pre-drift mixture model and the stable post-drift mixture model.",
+    		0, 0, Integer.MAX_VALUE);
+    
+    public FloatOption driftMagnitudeConditional = new FloatOption("driftMagnitudeConditional",
+    	    'D',
+    	    "Magnitude of the drift between the starting probability and the one after the drift."
+    		    + " Magnitude is expressed as the Hellinger distance [0,1]", 0.5, 1e-20, 0.9);
+    
+    public FloatOption precisionDriftMagnitude = new FloatOption("epsilon", 'e',
+    	    "Precision of the drift magnitude for p(x) (how far from the set magnitude is acceptable)",
+    	    0.01, 1e-20, 1.0);
+    
+    public FlagOption driftConditional = new FlagOption("driftConditional", 'c',
+    	    "States if the drift should apply to the conditional distribution p(y|x).");
+    
+    public FlagOption driftPriors = new FlagOption("driftPriors", 'p',
+    	    "States if the drift should apply to the prior distribution p(x). ");
+    
+    public MultiChoiceOption driftFunction = new MultiChoiceOption("driftFunction", 'f', 
+    		"Function to determine transition between concepts.", new String[]{
+                    "Linear", "Logistic"}, new String[]{"lin","log"}, 0);
+    
+    public IntOption numClassesPostOption = new IntOption("numClassesPost", 'P',
+            "The number of classes in the data stream and the number of models to include in the mixture model post-concept drift.",
             2, 2, Integer.MAX_VALUE);
     
     public IntOption modelRandomSeedOption = new IntOption("modelRandomSeed",
             'm', "Seed for random generation of model.", 1);
 
-    public IntOption instanceRandomSeedOption = new IntOption("instanceRandomSeed", 
-    		'i', "Seed for random generation of instances.", 1);
+    public IntOption instanceRandomSeedOption = new IntOption(
+            "instanceRandomSeed", 'i',
+            "Seed for random generation of instances.", 1);
 	
     protected InstancesHeader streamHeader;
-    protected MixtureModel mixtureModel;
+    protected MixtureModel mixtureModelPre, mixtureModelPost;
+    protected int numInstances;
     
     /**
 	 * @see moa.options.AbstractOptionHandler#prepareForUseImpl(moa.tasks.TaskMonitor, moa.core.ObjectRepository)
@@ -70,11 +106,23 @@ public class MixtureModelGenerator extends AbstractOptionHandler implements Inst
 	@Override
 	protected void prepareForUseImpl(TaskMonitor monitor, ObjectRepository repository)
 	{
-		generateHeader();
+		numInstances = 0;
 		
-		// Initialize mixture model
-		this.mixtureModel = new MixtureModel(this.numClassesOption.getValue(), this.numAttsOption.getValue(),
+		generateHeader(this.numClassesPreOption.getValue());
+		
+		// Initialize pre-concept drift mixture model
+		this.mixtureModelPre = new MixtureModel(this.numClassesPreOption.getValue(), this.numAttsOption.getValue(),
 				this.instanceRandomSeedOption.getValue(), this.modelRandomSeedOption.getValue());
+		
+		// Initialize post-concept drift mixture model
+		this.mixtureModelPost = new MixtureModel(this.numClassesPostOption.getValue(), this.numAttsOption.getValue(),
+				this.instanceRandomSeedOption.getValue(), this.modelRandomSeedOption.getValue());
+		
+		while(hellingerDistance(this.mixtureModelPre, this.mixtureModelPost) > this.precisionDriftMagnitude.getValue())
+		{
+			this.mixtureModelPost = new MixtureModel(this.numClassesPostOption.getValue(), this.numAttsOption.getValue(),
+					this.instanceRandomSeedOption.getValue(), this.modelRandomSeedOption.getValue());
+		}
 	}
 
 	/**
@@ -86,20 +134,28 @@ public class MixtureModelGenerator extends AbstractOptionHandler implements Inst
 	@Override
 	public Example<Instance> nextInstance()
 	{
-		return this.mixtureModel.nextInstance(this.getHeader());
+		if(this.numInstances > (this.burnInInstances.getValue() + this.driftDuration.getValue()))
+			return this.mixtureModelPost.nextInstance(this.getHeader());
+		else if(this.numInstances == (this.burnInInstances.getValue() + this.driftDuration.getValue()))
+		{
+			generateHeader(this.numClassesPostOption.getValue());
+			return this.mixtureModelPost.nextInstance(this.getHeader());
+		}
+		else
+			return this.mixtureModelPre.nextInstance(this.getHeader());
 	}
 	
 	/**
 	 * Generates the stream's header.
 	 */
-	private void generateHeader()
+	private void generateHeader(int numClasses)
 	{
 		FastVector<Attribute> attributes = new FastVector<Attribute>();
         for (int i = 0; i < this.numAttsOption.getValue(); i++) {
             attributes.addElement(new Attribute("att" + (i + 1)));
         }
         FastVector<String> classLabels = new FastVector<String>();
-        for (int i = 0; i < this.numClassesOption.getValue(); i++) {
+        for (int i = 0; i < numClasses; i++) {
             classLabels.addElement("class" + (i + 1));
         }
         attributes.addElement(new Attribute("class", classLabels));
@@ -124,6 +180,23 @@ public class MixtureModelGenerator extends AbstractOptionHandler implements Inst
 		return this.streamHeader;
 	}
 
+	/**
+	 * Uses Monte Carlo integration to calculate the Hellinger distance between the two argument mixture models.
+	 * 
+	 * @param mm1 the first mixture model
+	 * @param mm2 the second mixture model
+	 * @return the Hellinger distance between mm1 and mm2
+	 */
+	private double hellingerDistance(MixtureModel mm1, MixtureModel mm2)
+	{
+		double hDist = 1.0;
+		double monteCarlo = 0.0;
+		
+		// monte carlo integration
+		
+		return hDist - monteCarlo;
+	}
+	
 	/**
 	 * The MixtureModelGenerator can generate an infinite number of instances.
 	 * 
@@ -164,13 +237,14 @@ public class MixtureModelGenerator extends AbstractOptionHandler implements Inst
 	@Override
 	public void restart()
 	{
-		this.mixtureModel.restart(this.instanceRandomSeedOption.getValue(), this.modelRandomSeedOption.getValue());
+		this.mixtureModelPre.restart(this.instanceRandomSeedOption.getValue(), this.modelRandomSeedOption.getValue());
+		this.mixtureModelPost.restart(this.instanceRandomSeedOption.getValue(), this.modelRandomSeedOption.getValue());
 	}
 
 	 @Override
 	 public String getPurposeString()
 	 {
-		 return "Generates a data stream based on a mixture model.";
+		 return "Generates a data stream which features concept drift and is based on mixture models.";
 	 }
 	
 	/**
